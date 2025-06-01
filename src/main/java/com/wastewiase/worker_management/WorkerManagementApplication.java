56fd757407470f5b0a8236802df9da459ee0e756
Advay Banugariya <abanugariyaofficial@gmail.com>
2025-06-01 15:33:08 +0530

    first commit to the repo

---
+++ src/main/java/com/wastewiase/worker_management/WorkerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wastewiase.worker_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wastewiase/worker_management/WorkerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wastewiase.worker_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
